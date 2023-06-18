17ee7e2711463ef58342e825c2ea0d3fb91451b0
Dean <deang467@gmail.com>
2023-06-18 02:53:33 -0400

    added navbar. menu needs fixing

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function toggleDarkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
+}
+function displayNav() {
+    var x = document.getElementById("nav");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
   }
